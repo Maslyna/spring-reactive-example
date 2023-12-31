f610fbb957696045480af00c0f425169c1edb7cf
Maslyna <sanci324@gmail.com>
2023-12-31 10:55:58 +0100

    dev: exception handling added

---
+++ src/main/java/com/example/reactive/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.example.reactive.exception.handler;
+
+import com.example.reactive.exception.GlobalServiceException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(GlobalServiceException.class)
+    public ProblemDetail handleGlobalServiceException(GlobalServiceException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                e.getStatusCode(),
+                e.getReason() != null ? e.getReason() : e.getMessage()
+        );
+        problemDetail.setTitle("error");
+        return problemDetail;
+    }
+}
--- src/main/java/com/example/reactive/handler/PersonHandler.java
+++ src/main/java/com/example/reactive/handler/PersonHandler.java
@@ -1,8 +1,10 @@
 package com.example.reactive.handler;
 
 import com.example.reactive.entity.Person;
+import com.example.reactive.exception.GlobalServiceException;
 import com.example.reactive.service.PersonService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
@@ -20,43 +22,41 @@ public class PersonHandler {
 
     private final PersonService service;
 
-    public Mono<ServerResponse> createPerson(ServerRequest request) {
+    public Mono<ServerResponse> createPerson(final ServerRequest request) {
         return request.bodyToMono(Person.class)
                 .flatMap(service::save)
                 .flatMap(savedPerson -> ServerResponse.status(HttpStatus.CREATED).bodyValue(savedPerson));
     }
 
-    public Mono<ServerResponse> findAll(ServerRequest request) {
-        PageRequest pageRequest;
-        int page;
-        int size;
-
-        List<String> sortBy;
-        Sort.Direction direction;
+    public Mono<ServerResponse> findAll(final ServerRequest request) {
         try {
-            page = request.queryParam("page").map(Integer::parseUnsignedInt).orElse(0);
-            size = request.queryParam("size").map(Integer::parseUnsignedInt).orElse(5);
-        } catch (NumberFormatException e) {
-            return ServerResponse.badRequest().bodyValue("Invalid value for 'page' or 'size'");
+            PageRequest pageRequest = extractPageRequest(request);
+            return ServerResponse.ok().body(service.getPage(pageRequest), Page.class);
+        } catch (Exception e) {
+            throw new GlobalServiceException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
-        sortBy = request.queryParams().get("sortBy");
-        direction = request.queryParam("direction")
-                .map(Sort.Direction::fromString)
-                .orElse(Sort.Direction.DESC);
-
-        if (sortBy != null && !sortBy.isEmpty()) {
-            pageRequest = PageRequest.of(page, size, direction, sortBy.toArray(new String[0]));
-        } else {
-            pageRequest = PageRequest.of(page, size, direction);
-        }
-
-        return ServerResponse.ok().body(service.getPage(pageRequest), Person.class);
     }
 
-    public Mono<ServerResponse> findById(ServerRequest request) {
+    public Mono<ServerResponse> findById(final ServerRequest request) {
         UUID id = UUID.fromString(request.pathVariable("id"));
         return service.findById(id)
                 .flatMap(person -> ServerResponse.ok().bodyValue(person))
                 .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    private PageRequest extractPageRequest(final ServerRequest request) {
+        int page = request.queryParam("page").map(Integer::parseUnsignedInt).orElse(0);
+        int size = request.queryParam("size").map(Integer::parseUnsignedInt).orElse(5);
+
+        List<String> sortBy = request.queryParams().get("sortBy");
+        Sort.Direction direction = request.queryParam("direction")
+                .map(Sort.Direction::fromString)
+                .orElse(Sort.Direction.DESC);
+
+        if (sortBy != null && !sortBy.isEmpty()) {
+            return PageRequest.of(page, size, direction, sortBy.toArray(new String[0]));
+        } else {
+            return PageRequest.of(page, size);
+        }
     }
 }
