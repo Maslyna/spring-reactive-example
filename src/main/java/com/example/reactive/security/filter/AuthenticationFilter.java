a38d3bc88ecd7cab6946a4d1fd580413560e5027
Maslyna <sanci324@gmail.com>
2024-01-03 19:58:44 +0100

    dev: created auth filter for jwts

---
+++ src/main/java/com/example/reactive/security/filter/AuthenticationFilter.java
@@ -0,0 +1,51 @@
+package com.example.reactive.security.filter;
+
+import com.example.reactive.security.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Component
+public class AuthenticationFilter implements WebFilter {
+
+    private final JwtProvider jwtProvider;
+    private final ReactiveUserDetailsService userDetailsService;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String token = extractToken(exchange.getRequest());
+        if (StringUtils.hasText(token) && jwtProvider.isTokenValid(token)) {
+            String username = jwtProvider.getUsername(token);
+            return userDetailsService.findByUsername(username)
+                    .map(this::authenticate)
+                    .flatMap(authentication -> chain.filter(exchange)
+                            .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication)));
+        }
+        return chain.filter(exchange);
+    }
+
+
+    private String extractToken(ServerHttpRequest request) {
+        String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        if (StringUtils.hasText(authHeader) && authHeader.startsWith(JwtProvider.PREFIX)) {
+            return authHeader.substring(JwtProvider.PREFIX.length());
+        }
+        return null;
+    }
+
+    private Authentication authenticate(UserDetails user) {
+        return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+    }
+}
